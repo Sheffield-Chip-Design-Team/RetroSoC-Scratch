6f2fecb155a080947fa631a3724e64568c9d515b
Aiva <aivapersonal132@gmail.com>
2026-03-01 16:44:32 +0000

    More work on acc

--- HDL-Skills/Aiva/rtl/accumulator.v
+++ HDL-Skills/Aiva/rtl/accumulator.v
@@ -2,20 +2,25 @@ module accumulator (
   input wire clk,
   input wire acc_rst,
   input wire acc_en,
-  input wire acc_load,
-  input wire acc_shft,
-  input wire acc_shft_left,
-  input wire acc_inc,
-  input wire acc_dec,
+  input [23:0] opcode,
+  input wire op_rdy,
   input wire [7:0] acc_data_in,
   output reg [7:0] acc_out
 );
 
+parameter LDA = 'h888888;
+parameter INC = 'h888888;
+parameter DEC = 'h888888;
+
 always @(posedge clk) begin
   if (acc_en) begin
-    if (acc_rst) begin
-      acc_out <= 0;
-    end
+    case (opcode)
+      LDA: begin
+        acc_out <= acc_data_in;
+      end
+
+      default: 
+    endcase
   end
 end
   
